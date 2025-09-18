c0cb64ebe2250ba7ae0d038d2edeb3c575646b0b
figtisolucoes <infra@figti.com.br>
2025-09-18 10:20:14 -0300

    Operações

--- App.js
+++ App.js
@@ -1,43 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
 
-  function quandoClicar(){
-    console.log('FULANO')
+  const [primeiroNumero, setPrimeiroNumero] = useState('')
+  const [segundoNumero, setSegundoNumero] = useState('')
+  const [operador, setOperador] = useState('')
+  const [controleNumero, setControleNumero] = useState(1)
+
+  function setarNumero(numero){
+    if(controleNumero == 1){
+      setPrimeiroNumero(primeiroNumero + numero)
+    }else{
+      setSegundoNumero(segundoNumero + numero)
+    }
+  }
+
+  function selecionaOperacao(operacao){
+    setOperador(operacao)
+    setControleNumero(2)
   }
 
   return (
     <View style={styles.container}>
 
       <View style={{flexDirection:'row'}}>
-        <Text style={styles.font50}>1</Text>
-        <Text style={styles.font50}>+</Text>
-        <Text style={styles.font50}>2</Text>
-        <Text style={styles.font50}>=</Text>
-        <Text style={styles.font50}>3</Text>
+        <Text style={styles.font50}>{primeiroNumero}</Text>
+        <Text style={styles.font50}>{operador}</Text>
+        <Text style={styles.font50}>{segundoNumero}</Text>
       </View>
 
       <View style={{flexDirection: 'row', width:'100%'}}>
-        <TouchableOpacity style={styles.botao}>
+        <TouchableOpacity style={styles.botao} onPress={()=>setarNumero(1)}>
           <Text style={{fontSize:70}}>1</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botao}>
-          <Text style={{fontSize:70}}>1</Text>
+        <TouchableOpacity style={styles.botao} onPress={()=>setarNumero(2)}>
+          <Text style={{fontSize:70}}>2</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botao}>
-          <Text style={{fontSize:70}}>1</Text>
+        <TouchableOpacity style={styles.botao} onPress={()=>setarNumero(3)}>
+          <Text style={{fontSize:70}}>3</Text>
         </TouchableOpacity>
       </View>
 
       <View style={{flexDirection: 'row', width:'100%'}}>
-        <TouchableOpacity style={styles.botao}>
+        <TouchableOpacity style={styles.botao} onPress={()=>selecionaOperacao('+')}>
           <Text style={{fontSize:70}}>+</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botao}>
+        <TouchableOpacity style={styles.botao} onPress={()=>selecionaOperacao('-')}>
           <Text style={{fontSize:70}}>-</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botao}>
+        <TouchableOpacity style={styles.botao} onPress={()=>selecionaOperacao('*')}>
           <Text style={{fontSize:70}}>*</Text>
         </TouchableOpacity>
       </View>
@@ -50,8 +63,6 @@ export default function App() {
           <Text style={{fontSize:70}}>C</Text>
         </TouchableOpacity>
       </View>
-
-      
 
     </View>
   );
